cb4c0ce441ef285d5a457e7b837dbde25e026b1c
Ehson <ehsonboboev7@gmail.com>
2025-07-03 09:12:52 +0700

    Добавьте файлы проекта.

---
+++ ExamenBoboev/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenBoboev
+{
+
+
+    public class MaxMin
+    {
+
+
+        public static int NMax(double[] A, int N)
+        {
+
+            if (A.Length==0||N==0)
+            {
+                throw new ArgumentException("Введите корректные входные данные");
+            }
+
+            int maxindex = 0;
+            for (int i = 0; i < N; i++)
+            {
+
+                if (A[i] > A[maxindex])
+                {
+                    maxindex = i;
+                }
+
+            }
+            return maxindex;
+
+
+        }
+        public static int NMin(double[] A, int N)
+        {
+            
+            if (A.Length==0||N==0)
+            {
+                throw new ArgumentException("Введите корректные входные данные");
+            }
+
+            int minindex = 0;
+            for (int i = 0; i < N; i++)
+            {
+
+                if (A[i] < A[minindex])
+                {
+                    minindex = i;
+                }
+
+            }
+            return minindex;
+        }
+
+
+    }
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+
+            MaxMin arrayHelper = new MaxMin();
+            double[] f = { 2, 4, 5, 6, 7, 8 };
+            int s = MaxMin.NMax(f, f.Length);
+            Console.WriteLine("Индекс максимального число в масиве "+s);
+
+
+            int s1 = MaxMin.NMin(f, f.Length);
+            Console.WriteLine("Индекс минимального число в масиве "+s1);
+        }
+    }
+}
+
+
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,68 @@
+﻿using ExamenBoboev;
+using System.Reflection.PortableExecutable;
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+
+        /// <summary>
+        /// Коректный ввод данных 
+        /// </summary>
+        [TestMethod]
+        public void NMax()
+        {
+
+            double[] d = { 22, 234, -2, 24, 3 };
+
+            int result = MaxMin.NMin(d, d.Length);
+            int resultmax = MaxMin.NMax(d, d.Length);
+
+            Assert.AreEqual(2, result); // Проверка индекса минимального элемента
+            Assert.AreEqual(1, resultmax); // Проверка индекса максимального элемента
+
+        }
+
+
+
+        ///// <summary>
+        ///// Максимальное число 
+        ///// </summary>
+        //[TestMethod]
+        //public void NMax_Is_Actually_Max()
+        //{
+        //    double[] A = { 1.0, 7.0, 3.0 };
+        //    int index = MaxMin.NMax(A, A.Length);
+        //    Assert.IsTrue(A[index] == 7.0);
+        //}
+
+
+        /// <summary>
+        /// Пустые значение 
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Max()
+        {
+            double[] A = { };
+            var index = MaxMin.NMax(A, A.Length);
+            //int resultmax = MaxMin.NMax(d, d.Length);
+ 
+        }
+
+
+        /// <summary>
+        /// 0.1 не на нулевой позиции
+        /// </summary>
+        [TestMethod]
+        public void NMin_Not_Incorrect_Index()
+        {
+            double[] A = { 4.4, 2.2, 0.1 };
+            int result = MaxMin.NMin(A, A.Length);
+            Assert.AreNotEqual(0, result); 
+        }
+    }
+}
+
+
+
